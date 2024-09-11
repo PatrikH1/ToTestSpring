c01a7b2fb660bd8b7c513a6950ab5fb1c07e277d
Jonas Hulthén <patrik.hulthen@live.com>
2024-09-11 22:51:40 +0200

    Post added to make all better.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,10 +5,7 @@ import com.example.demo.user.UserDaoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -51,6 +48,13 @@ public class DemoApplication {
 	public User retriveUser(@PathVariable int id)
 	{
 		return service.findOne(id);
+	}
+
+	//method that posts a new user detail
+	@PostMapping("/users")
+	public void createUser(@RequestBody User user)
+	{
+		User sevedUser=service.save(user);
 	}
 
 	@GetMapping("/hello")
